55ce744e6bd9dd26fdd88228a996d3e37c8fede3
gnzlplcs <palaciosgm@outlook.com>
2022-03-21 17:40:45 -0500

    🎊 primer commit

---
+++ src/componentes/Testimonio.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Testimonio = () => {
+  return (
+    <div className="contenedor-testimonio">
+      <img
+        className="imagen-testimonio"
+        src={require("../imagenes/testimonio-emma.png")}
+        alt="Perfil de Emma"
+      />
+    </div>
+  );
+};
+
+export default Testimonio;
